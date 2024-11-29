49dca8e58a8925793e3d4ae0d96af8074601e9c5
dararod <dalejorod@gmail.com>
2024-11-29 17:55:01 -0400

    feat: context

---
+++ src/context/BasketContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from 'react';
+
+import type { BasketItem } from '../model/BasketItem';
+import { Basket } from '../model/Basket';
+
+export type BasketContextValue = {
+    basket: Basket;
+    addToBasket(bi: BasketItem): void;
+}
+
+export const BasketContext = createContext<BasketContextValue>({} as BasketContextValue);
+
+export function BasketContextProvider(props: { children: JSX.Element }): JSX.Element {
+    const [basket, setBasket] = useState(Basket.empty());
+
+    const addToBasket = (bi: BasketItem) => {
+        const next = basket.clone();
+        next.addItem(bi);
+        setBasket(next);
+    }
+
+    return (
+        <BasketContext.Provider value={{
+            basket,
+            addToBasket,
+        }}>
+            {props.children}
+        </BasketContext.Provider>
+    );
+};
+
+export default BasketContext;
