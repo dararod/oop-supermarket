b4ad796ddc8c502b7b759d93e1fc4308e496a0dd
dararod <dalejorod@gmail.com>
2024-11-29 16:17:11 -0400

    fix: test

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,9 +1,9 @@
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-// import { App } from '../src/components/App';
+import { App } from '../src/components/App';
 
-// test('renders hello world h1', () => {
-//   render(<App />);
-//   const h1El = screen.getByText(/Hello World/i);
-//   expect(h1El).toBeInTheDocument();
-// });
+test('renders header title', () => {
+  render(<App />);
+  const h1El = screen.getByText(/OOP Supermarket/i);
+  expect(h1El).toBeInTheDocument();
+});
