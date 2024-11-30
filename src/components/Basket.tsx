d71ceeb4871c08a25529b2a5c064e3394b795090
dararod <dalejorod@gmail.com>
2024-11-30 15:43:01 -0400

    feat: implement basket panel

---
+++ src/components/Basket.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./Header";
+
+export const Basket = () => {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import { useBasket } from "../hooks/use-basket";
-import { Basket } from "../model/Basket";
+import ShoppingCart from '~icons/custom/shopping-cart';
 
 export const Header = () => {
   const { basket } = useBasket();
-    return (
-      <header className="bg-red-600 px-4 h-12 flex items-center justify-between">
-        <h1 className="text-2xl text-white">
-          OOP Supermarket
-        </h1>
-        <div className="bg-red-700 text-white py-1 px-3 rounded font-semibold">
-          <span>${basket.totalBasket()}</span>
-        </div>
-      </header>
-    );
-  };
-  
+  return (
+    <header className="bg-red-600 px-4 h-12 flex items-center justify-between">
+      <a href="/">
+        <h1 className="text-2xl text-white">OOP Supermarket</h1>
+      </a>
+      <div className="bg-red-700 text-white py-1 px-3 rounded font-semibold">
+        <ShoppingCart />
+        <span>${basket.totalBasket()}</span>
+      </div>
+    </header>
+  );
+};
