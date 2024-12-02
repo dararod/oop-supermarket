61dc86f9a15cc7d6c49774a6e825645d10c2553e
dararod <dalejorod@gmail.com>
2024-12-02 13:58:44 -0400

    feat: implement category class to `IProduct` interface

--- src/model/products/Milk.ts
+++ src/model/products/Milk.ts
@@ -1,7 +1,9 @@
+import { Category } from "../categories/Category";
 import { Product } from "./Product";
 
+const dairyCategory = new Category("Dairy", "Dairy products", "dairy");
 export class Milk extends Product {
   constructor() {
-    super("Milk", 2.99, "MILK1L", '/assets/milk.png');
+    super("Milk", 2.99, "MILK1L", '/assets/milk.png', dairyCategory);
   }
 }
--- src/model/products/Pack.ts
+++ src/model/products/Pack.ts
@@ -1,11 +1,13 @@
 import { Product } from "./Product";
 
+import type { Category } from "../categories/Category";
+
 export abstract class Pack extends Product {
   public readonly quantity: number;
   public readonly unitPrice: number;
 
-  constructor(name: string, quantity: number, unitPrice: number, upc: string, imageUrl: string) {
-    super(name, quantity * unitPrice, upc, imageUrl)
+  constructor(name: string, quantity: number, unitPrice: number, upc: string, imageUrl: string, category: Category) {
+    super(name, quantity * unitPrice, upc, imageUrl, category);
 
     this.quantity = quantity;
     this.unitPrice = unitPrice;
