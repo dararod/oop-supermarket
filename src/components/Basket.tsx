c215ab0a7bd5f809b54fcfb493906188167a2160
dararod <dalejorod@gmail.com>
2024-12-07 12:15:45 -0400

    feat: capability to remove products

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -1,30 +1,47 @@
 import { Header } from "./Header";
 import { useBasket } from "../hooks/use-basket";
-import type { BasketItem } from "../model/BasketItem";
 
 export const Basket = () => {
-  const { basket, addToBasket, removeOneFromBasket, removeProductFromBasket } = useBasket();
-  console.log(basket);
+  const { basket, addToBasket, removeOneFromBasket, removeProductFromBasket } =
+    useBasket();
   return (
     <main className="h-screen">
       <Header />
       <div className="flex h-full items-center justify-center">
-        <div className="bg-zinc-300/20 h-96 w-96 flex flex-col items-center px-6 py-4 rounded-md">
+        <div className="bg-zinc-300/20 h-96 w-[400px] flex flex-col items-center px-6 py-4 rounded-md">
           <div className="flex justify-start w-full">
             <span>Order</span>
           </div>
           {basket.items.map((item, index) => (
             <div
-              className="grid grid-cols-3 gap-3 w-full"
+              className="grid grid-cols-3 gap-3 w-full py-1"
               key={`${item.product.upc}-${index}`}
             >
               <span className="col-span-1">{item.product.name}</span>
-                <div className="flex gap-2 justify-center">
-                  <button onClick={() => removeOneFromBasket(item)} className="px-1 bg-zinc-300/50 border border-zinc-500 rounded">-</button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => addToBasket(item)} className="px-1 bg-zinc-300/50 border border-zinc-500 rounded">+</button>
-                </div>
-                <span className="justify-self-end">${item.priceItem()}</span>
+              <div className="flex gap-2 justify-center">
+                <button
+                  onClick={() => removeOneFromBasket(item)}
+                  className="px-1 bg-zinc-300/50 border border-zinc-500 rounded"
+                >
+                  -
+                </button>
+                <span>{item.quantity}</span>
+                <button
+                  onClick={() => addToBasket(item)}
+                  className="px-1 bg-zinc-300/50 border border-zinc-500 rounded"
+                >
+                  +
+                </button>
+              </div>
+              <div className="flex gap-2 justify-self-end">
+                <span>${item.priceItem()}</span>
+                <button
+                  onClick={() => removeProductFromBasket(item.product.upc)}
+                  className="col-span-1 bg-red-400 border border-red-600 text-white px-2  rounded"
+                >
+                  Remove
+                </button>
+              </div>
             </div>
           ))}
         </div>
--- src/context/BasketContext.tsx
+++ src/context/BasketContext.tsx
@@ -1,74 +1,73 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
-import { Basket } from '../model/Basket';
+import { Basket } from "../model/Basket";
 
-import type { BasketItem } from '../model/BasketItem';
+import type { BasketItem } from "../model/BasketItem";
 
 export type BasketContextValue = {
-    basket: Basket;
-    addToBasket(bi: BasketItem): void;
-    removeOneFromBasket(bi: BasketItem): void;
-    removeProductFromBasket(bi: BasketItem): void;
-}
+  basket: Basket;
+  addToBasket(bi: BasketItem): void;
+  removeOneFromBasket(bi: BasketItem): void;
+  removeProductFromBasket(upc: string): void;
+};
 
-export const BasketContext = createContext<BasketContextValue>({} as BasketContextValue);
+export const BasketContext = createContext<BasketContextValue>(
+  {} as BasketContextValue
+);
 
 function loadLocalBasketOrDefault(): Basket {
-    const json = localStorage.getItem('basket');
-    return json ? Basket.deserialize(json) : Basket.empty();
+  const json = localStorage.getItem("basket");
+  return json ? Basket.deserialize(json) : Basket.empty();
 }
 
-export function BasketContextProvider(props: { children: JSX.Element }): JSX.Element {
-    const [basket, setBasket] = useState(loadLocalBasketOrDefault());
+export function BasketContextProvider(props: {
+  children: JSX.Element;
+}): JSX.Element {
+  const [basket, setBasket] = useState(loadLocalBasketOrDefault());
 
-    const addToBasket = (bi: BasketItem) => {
-        const next = basket.clone();
-        const productExistence = next.items.find(
-            (item) => item.product.upc === bi.product.upc
-          );
-          if (productExistence) {
-            productExistence.quantity = productExistence.quantity + 1;
-          } else {
-            next.addItem(bi);
-          }
-        setBasket(next);
-
-        localStorage.setItem('basket', next.serialize());
-    }
-
-    const removeOneFromBasket = (bi: BasketItem) => {
-        const next = basket.clone();
-        next.removeOneItem(bi);
-        setBasket(next);
-
-        localStorage.setItem('basket', next.serialize());
-    }
-
-    const removeProductFromBasket = (bi: BasketItem) => {
-        const next = basket.clone();
-        const productExistence = next.items.find(
-            (item) => item.product.upc === bi.product.upc
-          );
-          if (productExistence && productExistence.quantity - 1 > 0) {
-            productExistence.quantity = productExistence.quantity - bi.quantity;
-          } else {
-            next.addItem(bi);
-          }
-        setBasket(next);
-
-        localStorage.setItem('basket', next.serialize());
-    }
-
-    return (
-        <BasketContext.Provider value={{
-            basket,
-            addToBasket,
-            removeOneFromBasket,
-            removeProductFromBasket
-        }}>
-            {props.children}
-        </BasketContext.Provider>
+  const addToBasket = (bi: BasketItem) => {
+    const next = basket.clone();
+    const productExistence = next.items.find(
+      (item) => item.product.upc === bi.product.upc
     );
-};
+    if (productExistence) {
+      productExistence.quantity = productExistence.quantity + 1;
+    } else {
+      next.addItem(bi);
+    }
+    setBasket(next);
+
+    localStorage.setItem("basket", next.serialize());
+  };
+
+  const removeOneFromBasket = (bi: BasketItem) => {
+    const next = basket.clone();
+    next.removeOneItem(bi);
+    setBasket(next);
+
+    localStorage.setItem("basket", next.serialize());
+  };
+
+  const removeProductFromBasket = (upc: string) => {
+    const next = basket.clone();
+    next.removeItem(upc);
+    setBasket(next);
+
+    localStorage.setItem("basket", next.serialize());
+  };
+
+  return (
+    <BasketContext.Provider
+      value={{
+        basket,
+        addToBasket,
+        removeOneFromBasket,
+        removeProductFromBasket,
+      }}
+    >
+      {props.children}
+    </BasketContext.Provider>
+  );
+}
 
 export default BasketContext;
